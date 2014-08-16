02ca40d05d4f5f9c8709928dea7173a15426caa1
JSchwehn <jschwehn+wit@gmail.com>
2014-08-16 18:35:36 +0200

    Now handles special channels with two # Voting System is improved now. Votes and voters can be reset

--- main.go
+++ main.go
@@ -27,13 +27,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"             // todo use the log from go to get rid of the fmt.Print*
-	"net/textproto"   // Everything to handle string based protocols
-	"os"              // needed to interact with files
-	"regexp"          // ReExp lib
-	"sort"            // maps in go will not return the sequence as we put it in. We have to handle the sorting our self
-	"strings"         // Some string manipulations
-	"time"            // needed for the time.Sleep() method.
+	"log"           // todo use the log from go to get rid of the fmt.Print*
+	"net/textproto" // Everything to handle string based protocols
+	"os"            // needed to interact with files
+	"regexp"        // ReExp lib
+	"sort"          // maps in go will not return the sequence as we put it in. We have to handle the sorting our self
+	"strconv"       // convert from string to other formats like int
+	"strings"       // Some string manipulations
+	"time"          // needed for the time.Sleep() method.
 )
 
 const SLEEP = 500
@@ -42,38 +43,38 @@ const SLEEP = 500
 
 // global vars
 var (
-	Trace         *log.Logger // todo use the logger
-	Error         *log.Logger // todo use the logger
-	debug = true              // turn debugging on or off
+	Trace *log.Logger // todo use the logger
+	Error *log.Logger // todo use the logger
+	debug = true      // turn debugging on or off
 	/* Preg match to parse common messages we receive. Extracts 4 parts
-	 	- sender
-	 	- host of sender
-	 	- receiver
-	 	- message
-	 */
-privMsg       = regexp.MustCompile("^:([a-zA-Z0-9`_\\-]+)!.+@(.+)PRIVMSG (#[a-zA-Z0-9_]+) :(.*)$")
-	/* Preg match to parse direct messages we receive. Extracts 4 parts
-		- sender
-		- host of sender
-		- receiver
-		- message
+	- sender
+	- host of sender
+	- receiver
+	- message
 	*/
-directMsg     = regexp.MustCompile("^:([a-zA-Z0-9`_\\-]+)!.+@(.+)PRIVMSG ([a-zA-Z0-9_]+) :(.*)$")
+	privMsg = regexp.MustCompile("^:([a-zA-Z0-9`_\\-]+)!.+@(.+)PRIVMSG (#{1,2}[a-zA-Z0-9_]+) :(.*)$")
+	/* Preg match to parse direct messages we receive. Extracts 4 parts
+	- sender
+	- host of sender
+	- receiver
+	- message
+	*/
+	directMsg = regexp.MustCompile("^:([a-zA-Z0-9`_\\-]+)!.+@(.+)PRIVMSG ([a-zA-Z0-9_]+) :(.*)$")
 	/* Match a PING message, we have to answer with a PONG message. Extract  1 part
-	 	- payload which has to be send back
-	 */
-ping          = regexp.MustCompile("^PING :([a-zA-Z0-9\\.]+)$")
+	- payload which has to be send back
+	*/
+	ping = regexp.MustCompile("^PING :([a-zA-Z0-9\\.]+)$")
 	/* This reg ex will be used to detect if where successful connected to the IRC Server.
 	   Be aware that this can be differ from IRC Network to IRC Network
-	 */
-motd          = regexp.MustCompile("^:[a-zA-Z\\.]+\\s376(.*)") // end of motd
+	*/
+	motd = regexp.MustCompile("^:[a-zA-Z\\.]+\\s376(.*)") // end of motd
 	/* Sometimes the bot's nick has been taken - a channel split perhaps. In this case the should use a secondary nick
 	   and if this fails to, we try to a backtick to the nick until the conflict is resolved.
 	   TODO: use the seoondary nick
-	 */
-nickCollision = regexp.MustCompile("^:[a-zA-Z\\.]+\\s433(.*)")
+	*/
+	nickCollision = regexp.MustCompile("^:[a-zA-Z\\.]+\\s433(.*)")
 	/* This is how the bot matches for commands. Each command has to be prefixed with an ! */
-pCommand      = regexp.MustCompile("^!(\\w+)\\s*(.*)")
+	pCommand = regexp.MustCompile("^!(\\w+)\\s*(.*)")
 )
 
 /**
@@ -125,8 +126,9 @@ type Command struct {
  */
 type Bot struct {
 	activeAdmins map[string]bool  // list of successful logged in users
+	voted        map[string]bool  // list of people who had voted
 	suggestions  map[int]Title    // list of title suggestions
-	conn         *textproto.Conn  // the tcp socket
+conn         *textproto.Conn  // the tcp socket
 	writeChan    chan *IrcMessage // a go channel used to centralize the writing and to add a flood protection for the bot
 	config       *Configuration   // the config for the bot
 }
@@ -206,15 +208,13 @@ func (bot Bot) nickCollisionHandler() {
  */
 func (bot Bot) endOfMOTDHandler() {
 	// get the default channels from the config
-	for i, c := range bot.config.Channels {
+	for _, c := range bot.config.Channels {
 		bot.conn.Cmd("JOIN %s\r\n", c)
 		if debug {
 			fmt.Printf("... joining channel %s \n", c)
 		}
 		// if we have many channels to join - don't rush it.
-		if i%2 == 0 {
-			time.Sleep(SLEEP * time.Millisecond)
-		}
+		time.Sleep(SLEEP * time.Millisecond)
 	}
 
 }
@@ -331,19 +331,74 @@ func (bot Bot) handelCommand(cmd Command) {
 			bot.conn.Cmd("JOIN %s", cmd.rawParam)
 		}
 		// leaves a irc channel; admin pm only
-	case "leave": fallthrough
+	case "leave":
+		fallthrough
 	case "part":
 		if cmd.isPrivate && bot.isAdmin(cmd.sender) {
 			data := strings.Split(cmd.rawParam, " ")
 			bot.conn.Cmd("PART %s %s", data[0], data[1])
 		}
+	case "vote":
+		bot.handelVote(cmd.sender, cmd.rawParam)
+	case "resetVote":
+		if bot.isAdmin(cmd.sender) {
+			for i, suggestion := range bot.suggestions {
+				b := suggestion
+				b.numOfVotes = 1
+				bot.suggestions[i] = b
+			}
+			bot.ResetVotes()
+		}
+	case "resetTitle":
+		if bot.isAdmin(cmd.sender) {
+			bot.ResetVotes()
+			bot.ResetTitles()
+		}
+	case "del":
+		if bot.isAdmin(cmd.sender) {
+			pos, err := strconv.Atoi(cmd.rawParam)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			//bot.suggestions[pos]
+			delete(bot.suggestions, pos)
+		}
 		// sends a hardcoded message - used for testing
 	case "fisch":
 		bot.sendMessage(cmd.answerTo, "Fischers Frize hat blaue Brautkleider.")
-		// help disabled due channel flooding :/
-		//	case "help":
-		//		bot.showHelp(cmd.receiver)
 	}
+}
+
+func (bot Bot) ResetTitles() {
+	for s := range bot.suggestions {
+		delete(bot.suggestions, s)
+	}
+}
+
+func (bot Bot) ResetVotes() {
+	for v := range bot.voted {
+		delete(bot.voted, v)
+	}
+}
+
+func (bot Bot) handelVote(user, id string) {
+	if bot.HasVoted(user) {
+		return
+	}
+	pos, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	b := bot.suggestions[pos]
+	b.numOfVotes++
+	if bot.isAdmin(user) {
+		bot.voted[user] = true
+	}
+
+	bot.suggestions[pos] = b
+	fmt.Print("Votes %s, for %s", bot.suggestions[pos].numOfVotes, bot.suggestions[pos].titleSuggestion)
 }
 
 /**
@@ -386,6 +441,13 @@ func (bot Bot) isAdmin(nick string) bool {
 	return false
 }
 
+func (bot Bot) HasVoted(nick string) bool {
+	if bot.voted[nick] {
+		return true
+	}
+	return false
+}
+
 /**
  * Set the nick for the bot
  */
@@ -406,6 +468,10 @@ func (bot Bot) channelWriter() {
 	bot.conn.Cmd("PRIVMSG %s :%s\r\n", msg.receiver, msg.message)
 }
 
+func (bot Bot) writeCfg() {
+
+}
+
 /**
  * Sets the Nick after we connected to the server
  * Receives everything the server trows at the bot and it go'ed the string parser and channelWriter
@@ -423,36 +489,12 @@ func (bot Bot) startBot() {
 	}
 }
 
-/**
- *  Send a small help to a user.
- *  This is disabled because it can be abused to get the bot kicked due flooding
- */
-//func (bot Bot) showHelp(receiver string) {
-//	var help = []string{
-//		"- !topic <topic string>   : * Suggest a topic",
-//		"- !title <topic string>   : * Suggest a topic",
-//		"- !obeyMe <pass string>   : Will only be recognised if sent as a direct message. Used to authenticate a user with a provided password",
-//	}
-//	var adminHelp = []string{
-//		"-",
-//		"After a user has been successful authenticated he has access to a admin commands. Each admin command *must* be send as a direct message",
-//		"- !resetTopicList            : Flushes the topic list",
-//		"- !addMaster <nick string> <pass string>  : adds a new",
-//		"- !delMaster <nick string>   : Removes a master from the system",
-//		"- !removeTopic <topicId int> : Removes a topic",
-//		"- !quit <message string>     : Shutdown the bot and leave a goodbye message",
-//		"-",
-//	}
-//
-//	for _, element := range help {
-//		bot.sendMessage(receiver, element+"\r\n")
-//	}
-//	for _, element := range adminHelp {
-//		bot.sendMessage(receiver, element+"\r\n")
-//	}
-//}
+func (bot Bot) loadCfg(file string) {
+
+}
 
 func main() {
+
 	// read log file todo: move in function
 	cfgFile, _ := os.Open("bot.json")
 	decoder := json.NewDecoder(cfgFile)
@@ -467,6 +509,7 @@ func main() {
 	// Build bot
 	bot := new(Bot)
 	bot.activeAdmins = make(map[string]bool)
+	bot.voted = make(map[string]bool)
 	bot.suggestions = make(map[int]Title)
 	bot.writeChan = make(chan *IrcMessage)
 	bot.config = &cfg
